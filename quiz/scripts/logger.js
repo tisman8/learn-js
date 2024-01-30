cccf632dd4415e5b0bb5b7258e76c1ea2ace110a
Tracy Isman <tracyisman@gmail.com>
2024-01-30 12:23:54 -0500

    logger quiz

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,17 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    console.log(this.errMsg);
+}
+
+const fakeObject = {
+    msg: 'Some message that will not be logged',
+    errMsg: 'This message will get logged'
+}
+
+const fakeObject2 = {
+    propertyThis: 'not gonna get logged',
+    errMsg: 'Hello! How are you?'
+}
+
+logMsg.call(fakeObject);
+logMsg.call(fakeObject2);
