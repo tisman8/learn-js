38bb4d6c26df89ccb0a2d840f997abfe04a7c5eb
Tracy Isman <tracyisman@gmail.com>
2024-02-01 15:38:52 -0500

    manipulate quiz

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,29 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createButtonNode(text, event, listener) {
+  let buttonNode = document.createElement("button");
+  let buttonNodeText = createTxtNode(text);
+  buttonNode.appendChild(buttonNodeText);
+  buttonNode.addEventListener(event, listener);
+  return buttonNode;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createButtonNode("Edit text", "click", function() {
+      edit(col1);
+    }));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
+}
+
+function edit(node) {
+  let newNode = document.createElement("input");
+  newNode.type = 'text';
+  newNode.placeholder = 'Enter Cell (x, y) ...';
+  node.replaceChild(newNode, node.childNodes[0]);
 }
